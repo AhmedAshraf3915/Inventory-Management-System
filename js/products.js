f7472013973208891bd493924954de72e31a9b3a
Ahmed Ashraf <ahmed31009358@f-eng.tanta.edu.eg>
2026-03-22 00:07:20 +0200

    completed [dashboard-product-productModal ]with responsive design] and added modal functionality as utility

--- js/products.js
+++ js/products.js
@@ -1,2 +1,35 @@
 renderNavbar("Products");
 renderFooter()
+
+// ^ Modal Actions
+const addProductBtn = document.querySelector("#addProductBtn")
+const editProductBtn = document.querySelector("#editProductBtn")
+const modal = document.querySelector("#modal")
+const modalOverlay = document.querySelector('#modalOverlay')
+const cancelModal = document.querySelector("#cancelBtn")
+
+
+addProductBtn.addEventListener('click', function () {
+	showMoal()
+})
+
+editProductBtn.addEventListener('click', function () {
+	showMoal()
+})
+
+cancelModal.addEventListener("click", function () {
+	closeModal()
+})
+
+modalOverlay.addEventListener('click', function (e) {
+	if (e.target !== modal) {
+		closeModal()
+	}
+})
+document.addEventListener('keydown', function (e) {
+	console.log(e)
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal();
+	}
+})
+
